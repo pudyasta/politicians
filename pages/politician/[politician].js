f65dcccfc036665f6ea7e4c4ebe085b7656f63db
pudy <pudyastasatria@gmail.com>
2021-09-10 14:50:11 +0700

    adding 404 page

--- pages/politician/[politician].js
+++ pages/politician/[politician].js
@@ -24,7 +24,7 @@ export const getStaticProps = async (context) => {
     "fields.slug": context.params.politician,
   });
 
-  if (!items) {
+  if (!items.length) {
     return {
       redirect: {
         destination: "/",
