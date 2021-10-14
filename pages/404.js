aa5301aab632b7965de95cbc080120a8d4333deb
Pudyasta Satria <satriadevops@gmail.com>
2021-10-14 17:43:26 +0700

    backup commit

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className="text-4xl">404</h1>
+      <h4 className="text-xl">
+        Oopps your page is not exist. You'll e redirecting to the home page..
+      </h4>
+    </div>
+  );
+}
